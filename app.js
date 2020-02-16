fdb54175f8076779e811615af3a4ecd4e2001328
Bharathi <baroselva@gmail.com>
2020-02-16 16:02:44 +0530

    third commit

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(process.env.PORT ||3000);
 
 
 app.get('/',function(req,res){
